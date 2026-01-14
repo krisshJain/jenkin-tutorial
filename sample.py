97c1464930938f600de8b2a75231906b2a7fa135
krisshJain <krish.17958@sakec.ac.in>
2026-01-14 12:04:34 +0530

    feat: add intentional code smells and security risks for SonarCloud testing

--- sample.py
+++ sample.py
@@ -1,10 +1,23 @@
 import os
 
+# 1. SECURITY RISK: Hardcoded 'password' (SonarCloud will flag this)
+SECRET_KEY = "admin123" 
+
 # Jenkins will pass these as environment variables
 a = int(os.getenv('VAL_A', 10))
 b = int(os.getenv('VAL_B', 5))
 
+# 2. CODE SMELL: Unused variable (SonarCloud will flag this)
+unused_calculation = a + b + 100 
+
 print("--- Jenkins Math Result ---")
-print(f"Adding {a} + {b} = {a + b}")
+
+# 3. BAD PRACTICE: Using a broad Exception (SonarCloud will flag this)
+try:
+    print(f"Adding {a} + {b} = {a + b}")
+    print(f"Dividing {a} / {b} = {a / b}")
+except Exception as e:
+    print("Something went wrong")
+
 print(f"Multiplying {a} * {b} = {a * b}")
-print("--- Build Successful ---")
+print("--- Build Successful ---")v
